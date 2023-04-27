4a10cc2b93d52bebf18d640465731e36808193be
Slawomir Siudek <slawomir@siudek.net>
2023-04-27 14:19:06 +0200

    Migration to Spring

---
+++ src/main/java/net/siudek/myphotos/MyphotosApplication.java
@@ -0,0 +1,13 @@
+package net.siudek.myphotos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyphotosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyphotosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/siudek/myphotos/MyphotosApplicationTests.java
@@ -0,0 +1,13 @@
+package net.siudek.myphotos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyphotosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
